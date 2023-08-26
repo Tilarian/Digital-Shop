3177bbf8f5906aef9488234b36655a3a81aaeef2
Leonardo Gutierrez <tilarian@outlook.com>
2023-08-26 19:58:05 -0300

    Se agrega componente y Json

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,9 +1,13 @@
 import Container from 'react-bootstrap/Container';
 
+import { ItemCount } from './ItemCount';
+
+
 export const ItemListContainer = (props) => {
     return (
         <Container class="mt-4">
             <h1>{props.greeting}</h1>
+            <ItemCount />
         </Container>
     );
 
