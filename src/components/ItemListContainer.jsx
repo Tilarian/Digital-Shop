3895e54b0ca21db82f62f5243b6d2edfde4665d6
Leonardo Gutierrez <tilarian@outlook.com>
2023-08-26 21:49:56 -0300

    Se agrega estilos e imagenes a las cards

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,13 +1,42 @@
+import { useState, useEffect } from "react";
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-import { ItemCount } from './ItemCount';
+import data from "../data/products.json";
 
 
 export const ItemListContainer = (props) => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => resolve(data), 2000);
+        });
+        promise.then(data => setProducts(data));
+    }, [])
+
+    console.log(products);
+
     return (
         <Container class="mt-4">
             <h1>{props.greeting}</h1>
-            <ItemCount />
+            <div class="cards">
+                {products.map(product => (
+
+                    <Card key={product.id} style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={product.url} />
+                        <Card.Body>
+                            <Card.Title>{product.name}</Card.Title>
+                            <Card.Text>
+                                {product.category}
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
+
+                ))}
+            </div>
         </Container>
     );
 
