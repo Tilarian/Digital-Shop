7dcbcbdd0f3aa3d98505fe19cc963aef6f7f2cf1
Leonardo Gutierrez <tilarian@outlook.com>
2023-09-22 14:27:29 -0300

    .

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,36 +1,40 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
-import {getFirestore,doc,getDoc} from "firebase/firestore";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 
 
 import { ItemDetail } from "./ItemDetail";
 
 
 export const ItemDetailContainer = (props) => {
-    const [product, setProduct] = useState(null);
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const { id } = useParams();
-  
-    
+  const { id } = useParams();
 
-    useEffect(() => {
-        const db = getFirestore();
-    
-        const refDoc = doc(db, "Items", id);
-    
-        getDoc(refDoc).then((snapshot) => {
-          setProduct({ id: snapshot.id, ...snapshot.data() });
-        });
-      }, [id]);
 
-    if (!product) return <div>load...</div>
 
-    return (
-        <Container className="mt-4">
-            <h1>Detalles</h1>
-            <ItemDetail product={ product } />
-        </Container>
-    );
+  useEffect(() => {
+    const db = getFirestore();
+
+    const refDoc = doc(db, "Items", id);
+
+    getDoc(refDoc).then((snapshot) => {
+      setProduct({ id: snapshot.id, ...snapshot.data() });
+    })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [id]);
+
+  if (loading) return <div>loading...</div>
+
+  return (
+    <Container className="mt-4">
+      <h1>Detalles</h1>
+      <ItemDetail product={product} />
+    </Container>
+  );
 
 };
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,40 +1,45 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
-import {getFirestore,collection,getDocs} from "firebase/firestore";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 import { ItemList } from "./ItemList";
 
 
 export const ItemListContainer = (props) => {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true)
 
-    const { id } = useParams();
-    
-    
-    useEffect(() => {
-        const db = getFirestore();
-    
-        const refCollection = collection(db, "Items");
-    
-        getDocs(refCollection).then((snapshot) => {
-          if (snapshot.size === 0) console.log("no results");
-          else
-            setProducts(
-              snapshot.docs.map((doc) => {
-                return { id: doc.id, ...doc.data() };
-              })
-            );
-        });
-      }, [id]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const db = getFirestore();
+
+    const refCollection = collection(db, "Items");
+
+    getDocs(refCollection).then((snapshot) => {
+      if (snapshot.size === 0) console.log("no results");
+      else
+        setProducts(
+          snapshot.docs.map((doc) => {
+            return { id: doc.id, ...doc.data() };
+          })
+        );
+    })
+    .finally(() => {
+      setLoading(false)
+    })
+  }, [id]);
+
+  if (loading) return <div>Loading...</div>
 
 
-    return (
-        <Container className="mt-4">
-            <h1>{props.greeting}</h1>
-            <div className="cards">
-                <ItemList products={products} />
-            </div>
-        </Container>
-    );
+  return (
+    <Container className="mt-4">
+      <h1>{props.greeting}</h1>
+      <div className="cards">
+        <ItemList products={products} />
+      </div>
+    </Container>
+  );
 
 };
